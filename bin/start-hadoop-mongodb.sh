7937f9c28649d1d473698c359d22a31f8c50fa8a
unknown <Jiaan>
2014-04-17 09:57:00 -0400

    update scripts

--- bin/start-hadoop-mongodb.sh
+++ bin/start-hadoop-mongodb.sh
@@ -6,6 +6,7 @@ HADOOP_PATH=~/hadoop-data
 
 ### ALERT! PREVIOUS RESULTS ARE REMOVED! ###
 killall java; 
+killall mongod;
 rm -fr $MONGODB_DBPATH; rm -fr $MONGODB_LOGPATH; rm -fr $HADOOP_PATH
 mkdir -p $MONGODB_DBPATH; mkdir -p $MONGODB_LOGPATH; mkdir -p $HADOOP_PATH
 
