ff0cc5ca1295019403ebfc4071070e269e93fd16
unknown <Jiaan>
2014-04-17 14:42:26 -0400

    add files

--- text_processing/getTFDF.py
+++ text_processing/getTFDF.py
@@ -38,61 +38,65 @@ def import2mongo(filepath):
     dfdict_uni, dfdict_bi, dfdict_tri = [defaultdict(float)] * 3 # document frequencies of terms
     tf_uni, tf_bi, tf_tri = [[]] * 3 # list of documents
     chardoclist = []
-    with codecs.open(filepath, encoding='utf8') as fin:
-        old_key = None # tracking doc_id
-        tfdict_uni, tfdict_bi, tfdict_tri = [{}] * 3 # term frequency dictionary for each document
-        chardict = defaultdict(float)
-        for line in fin:
-            if line:
-                this_key, term, tf = line.split('\t')
-                term = term.replace('.', '-').replace('$', '-') # BSON doesn't allow '.' and '$'
-                if this_key != old_key and old_key:
-                    tf_uni.append({"_id":old_key, "freq":tfdict_uni, "prob":freq2prob(tfdict_uni)})
-                    tf_bi.append({"_id":old_key, "freq":tfdict_bi, "prob":freq2prob(tfdict_bi)})
-                    tf_tri.append({"_id":old_key, "freq":tfdict_tri, "prob":freq2prob(tfdict_tri)})
-                    chardoclist.append({"_id":old_key, "freq":chardict, "prob":freq2prob(chardict)})
-                    tfdict_uni, tfdict_bi, tfdict_tri = [{}] * 3
-                    chardict = defaultdict(float)
-                    count += 1
-                    if count > 2000: # 2000 docs as a batch
-                        db.tf_1.insert(tf_uni)
-                        db.tf_2.insert(tf_bi)
-                        db.tf_3.insert(tf_tri)
-                        db.cf.insert(chardoclist)
-                        # clear memory & count
-                        tf_uni, tf_bi, tf_tri = [[]] * 3
-                        chardoclist = []
-                        count = 0
-
-                old_key = this_key
-                # update tf & df
-                if term.count(' ') == 0:
-                    tfdict_uni[term] = float(tf)
-                    dfdict_uni[term] += 1
-                    for char in term:
-                        chardict[char] += float(tf)
-                elif term.count(' ') == 1:
-                    tfdict_bi[term] = float(tf)
-                    dfdict_bi[term] += 1
-                elif term.count(' ') == 2:
-                    tfdict_tri[term] = float(tf)
-                    dfdict_tri[term] += 1
-
-        # dont forget last doc
-        tf_uni.append({"_id":old_key, "freq":tfdict_uni, "prob":freq2prob(tfdict_uni)})
-        tf_bi.append({"_id":old_key, "freq":tfdict_bi, "prob":freq2prob(tfdict_bi)})
-        tf_tri.append({"_id":old_key, "freq":tfdict_tri, "prob":freq2prob(tfdict_tri)})
-        chardoclist.append({"_id":old_key, "freq":chardict, "prob":freq2prob(chardict)})
-        # insert regardless of count
-        db.tf_1.insert(tf_uni)
-        db.tf_2.insert(tf_bi)
-        db.tf_3.insert(tf_tri)
-        db.cf.insert(chardoclist)
-
-    # save df (document frequencies) to collections ('df_1','df_2','df_3')
-    db.df_1.insert(reshape(dfdict_uni))
-    db.df_2.insert(reshape(dfdict_bi))
-    db.df_3.insert(reshape(dfdict_tri))
+    
+    try:
+        with codecs.open(filepath, encoding='utf8') as fin:
+            old_key = None # tracking doc_id
+            tfdict_uni, tfdict_bi, tfdict_tri = [{}] * 3 # term frequency dictionary for each document
+            chardict = defaultdict(float)
+            for line in fin:
+                if line:
+                    this_key, term, tf = line.split('\t')
+                    term = term.replace('.', '-').replace('$', '-') # BSON doesn't allow '.' and '$'
+                    if this_key != old_key and old_key:
+                        tf_uni.append({"_id":old_key, "freq":tfdict_uni, "prob":freq2prob(tfdict_uni)})
+                        tf_bi.append({"_id":old_key, "freq":tfdict_bi, "prob":freq2prob(tfdict_bi)})
+                        tf_tri.append({"_id":old_key, "freq":tfdict_tri, "prob":freq2prob(tfdict_tri)})
+                        chardoclist.append({"_id":old_key, "freq":chardict, "prob":freq2prob(chardict)})
+                        tfdict_uni, tfdict_bi, tfdict_tri = [{}] * 3
+                        chardict = defaultdict(float)
+                        count += 1
+                        if count > 2000: # 2000 docs as a batch
+                            db.tf_1.insert(tf_uni)
+                            db.tf_2.insert(tf_bi)
+                            db.tf_3.insert(tf_tri)
+                            db.cf.insert(chardoclist)
+                            # clear memory & count
+                            tf_uni, tf_bi, tf_tri = [[]] * 3
+                            chardoclist = []
+                            count = 0
+    
+                    old_key = this_key
+                    # update tf & df
+                    if term.count(' ') == 0:
+                        tfdict_uni[term] = float(tf)
+                        dfdict_uni[term] += 1
+                        for char in term:
+                            chardict[char] += float(tf)
+                    elif term.count(' ') == 1:
+                        tfdict_bi[term] = float(tf)
+                        dfdict_bi[term] += 1
+                    elif term.count(' ') == 2:
+                        tfdict_tri[term] = float(tf)
+                        dfdict_tri[term] += 1
+    
+            # dont forget last doc
+            tf_uni.append({"_id":old_key, "freq":tfdict_uni, "prob":freq2prob(tfdict_uni)})
+            tf_bi.append({"_id":old_key, "freq":tfdict_bi, "prob":freq2prob(tfdict_bi)})
+            tf_tri.append({"_id":old_key, "freq":tfdict_tri, "prob":freq2prob(tfdict_tri)})
+            chardoclist.append({"_id":old_key, "freq":chardict, "prob":freq2prob(chardict)})
+            # insert regardless of count
+            db.tf_1.insert(tf_uni)
+            db.tf_2.insert(tf_bi)
+            db.tf_3.insert(tf_tri)
+            db.cf.insert(chardoclist)
+    
+        # save df (document frequencies) to collections ('df_1','df_2','df_3')
+        db.df_1.insert(reshape(dfdict_uni))
+        db.df_2.insert(reshape(dfdict_bi))
+        db.df_3.insert(reshape(dfdict_tri))
+    except:
+        pass
 
 
 if __name__ == '__main__':
